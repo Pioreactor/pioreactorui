6967bedde33e999b1b3aacca87a6e635a02461de
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-12-21 12:44:10 -0500

    move export folder'

--- backend/child_tasks/db_export.js
+++ backend/child_tasks/db_export.js
@@ -6,7 +6,7 @@ process.on('message', function(options) {
     tables = tables.filter(t => options.datasetCheckbox[t])
     cmd_tables = tables.map(s => "--tables " + s).join(" ")
     filename = "export" + Math.floor(Date.now() / 1000) + ".zip"
-    command = ["pio", "run", "download_experiment_data", "--experiment", `"${options.experimentSelection}"`, "--output", "/home/pi/pioreactorui/backend/public/" + filename, cmd_tables].join(" ")
+    command = ["pio", "run", "download_experiment_data", "--experiment", `"${options.experimentSelection}"`, "--output", "/home/pi/pioreactorui/backend/build/static/exports/" + filename, cmd_tables].join(" ")
     console.log(command)
     exec(command, (error, stdout, stderr) => {
         console.log(stdout)
