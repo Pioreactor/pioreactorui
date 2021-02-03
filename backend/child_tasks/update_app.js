5e642945978ef75953c2a5caed2161b6f26a5475
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-02-02 20:32:51 -0500

    better deploy?

--- backend/child_tasks/update_app.js
+++ backend/child_tasks/update_app.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 
 process.on('message', function(v) {
     // pio update restarts the webserver...
-    command = "pio update && pios sync"
+    command = "pio update --app && pios update"
     console.log(command)
     exec(command, (error, stdout, stderr) => {
         console.log(stdout)
@@ -14,6 +14,10 @@ process.on('message', function(v) {
         } else {
             process.send(true);
         }
+
+        // now that we have sent a confirmation, also update the UI...
+        command = "pio update --ui"
+        exec(command)
         process.exit(0)
     });
 });
