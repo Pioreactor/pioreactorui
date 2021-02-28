9a3e89236d04b1c6983005e1c503d7971277ef8d
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-02-28 10:28:16 -0500

    fix download data

--- backend/child_tasks/db_export.js
+++ backend/child_tasks/db_export.js
@@ -5,9 +5,10 @@ process.on('message', function(options) {
     tables = Object.keys(options.datasetCheckbox)
     tables = tables.filter(t => options.datasetCheckbox[t])
     experiment = options.experimentSelection
-    cmd_tables = tables.map(s => "--tables " + s)
+    cmd_tables = tables.map(s => ["--tables", s]).flat()
     filename = `export_${experiment.replace(" ", "_")}_${Math.floor(Date.now() / 1000).toString()}.zip`
-    execFile("pio", ["run", "download_experiment_data", "--experiment", `"${experiment}"`, "--output", `"/home/pi/pioreactorui/backend/build/static/exports/${filename}"`] + cmd_tables, (error, stdout, stderr) => {
+    console.log(["run", "download_experiment_data", "--experiment", `"${experiment}"`, "--output", `"/home/pi/pioreactorui/backend/build/static/exports/${filename}"`].concat(cmd_tables))
+    execFile("pio", ["run", "download_experiment_data", "--experiment", `"${experiment}"`, "--output", `"/home/pi/pioreactorui/backend/build/static/exports/${filename}"`].concat(cmd_tables), (error, stdout, stderr) => {
         console.log(stdout)
         console.log(stderr)
         if (error) {
