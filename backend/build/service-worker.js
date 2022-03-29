522e9619615f93e72f006ff76c7d637006e4a2be
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-03-29 14:55:42 -0400

    initial commit to add more experiment metadata into the db

--- backend/build/service-worker.js
+++ backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6c6c7add56be64e2c563eb5daea9745.js"
+  "/precache-manifest.950e2dff982640f0e266daf16c52bf83.js"
 );
 
 self.addEventListener('message', (event) => {
