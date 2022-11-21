bf958f9fc152d3cf2909759d72faf9847c5c1fca
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-11-21 09:59:28 -0500

    fix window location on link

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcee40537c619b66d676331a242b1ba6.js"
+  "/precache-manifest.12b8cfd88cbe0cc825fb632c16528663.js"
 );
 
 self.addEventListener('message', (event) => {
