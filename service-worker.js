fafa31e60fb3cc09d0e7029d3272ca49ea7e3729
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-12-27 13:12:28 -0500

    adding pwm dc to export data pag

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f68ef17db7d29e0e5138496bd751e38c.js"
+  "/precache-manifest.5e709619ad77e80c03050885ff48e5c2.js"
 );
 
 self.addEventListener('message', (event) => {
