ed849ab2b5d100a1a781f8528a515b6af3fdd96a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-01-12 14:16:18 -0500

    adding error message if unable to connect to mqtt

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c4b24a0d8e6249d9cb57454f7c7bdb2.js"
+  "/precache-manifest.d4e7dac92ad6e1ac1edfffcc4e4050c2.js"
 );
 
 self.addEventListener('message', (event) => {
